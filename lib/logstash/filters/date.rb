da6bd091bce6e7817da953f16a9cdf79aabb1d93
Pier-Hugues Pellerin <phpellerin@gmail.com>
2015-10-07 11:12:29 -0400

    remove filter

--- lib/logstash/filters/date.rb
+++ lib/logstash/filters/date.rb
@@ -204,7 +204,7 @@ class LogStash::Filters::Date < LogStash::Filters::Base
   public
   def filter(event)
     @logger.debug? && @logger.debug("Date filter: received event", :type => event["type"])
-    return unless filter?(event)
+    
     @parsers.each do |field, fieldparsers|
       @logger.debug? && @logger.debug("Date filter looking for field",
                                       :type => event["type"], :field => field)
