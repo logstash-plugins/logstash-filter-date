17bbe4a50c20a634deb73b45dbaf10e3d1d299f6
Joao Duarte <jsvduarte@gmail.com>
2020-03-11 10:38:22 +0000

    move testing to centralized travis configuration
    * add custom setup.sh
    * upgrade gradle to 5.6.4
    * make build.gradle gradle 5 compliant
    * move ci folder to .ci

---
+++ .ci/setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# This is intended to be run inside the docker container as the command of the docker-compose.
+
+env
+
+set -ex
+
+bundle exec rake gradle.properties
+./gradlew assemble
